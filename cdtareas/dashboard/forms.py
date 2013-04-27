29307d5a3f77782127f95cc33fbde23b9526a3dd
Carlos Do Carmo <cdocarmo@cdsoft.com.uy>
2013-04-27 14:46:13 -0300

    Modificacion Vista Login-Registro

--- cdtareas/dashboard/forms.py
+++ cdtareas/dashboard/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 # from django.contrib.auth.models import User
 from datetime import date, datetime, timedelta
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class RegisterForm(UserCreationForm):
     
@@ -14,7 +14,16 @@ class RegisterForm(UserCreationForm):
               'password1':u'Escribe un buen password.',
               'password2':u'Ahora repitelo, para verificar.'
     }
+
+    firstname = forms.CharField(label=_(u'Nombre'), max_length=30, 
+                               widget=forms.TextInput(attrs={'class':'input-text'}), help_text = ayudas['username'])
+
     
+    lastname = forms.CharField(label=_(u'Apellido'), max_length=30, 
+                           widget=forms.TextInput(attrs={'class':'input-text'}), help_text = ayudas['username'])
+
+
+
     username = forms.CharField(label=_(u'Usuario'), max_length=30, 
                                widget=forms.TextInput(attrs={'class':'input-text'}), help_text = ayudas['username'])
     email = forms.EmailField(label = "Email personal", widget=forms.TextInput(attrs={'class':'input-text'}), 
@@ -22,22 +31,30 @@ class RegisterForm(UserCreationForm):
     #override password1 and password2 from UserCreationForm
     password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={'class':'input-text'}), 
                                 help_text=ayudas['password1'])
-    password2 = forms.CharField(label=_("Confirmacion Password"), widget=forms.PasswordInput(attrs={'class':'input-text'}),
+    password2 = forms.CharField(label=_("Confirmar Password"), widget=forms.PasswordInput(attrs={'class':'input-text'}),
         help_text=ayudas['password2'])
-    
-    def as_br(self):
-        """
-        GF
-        Based in as_p, from superclass forms.py.
-        Returns this form rendered as HTML <br />s.
-        """
-        return self._html_output(
-            normal_row = u'%(label)s%(field)s<br />',
-            error_row = u'%s',
-            row_ender = '',
-            help_text_html = u' <span class="helptext">%s</span>',
-            errors_on_separate_row = True)
     
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("firstname", "lastname", "username", "email", "password1", "password2")
+
+class LoginForm(AuthenticationForm):
+    
+    ayudas = {
+              'username':u'Ingresa el nombre de tu empresa o el tuyo propio si ofreces un servicio particular.',
+              'email':u'Ingresa una direcci\xf3n, si corresponde.',
+              'password1':u'Escribe un buen password.',
+              'password2':u'Ahora repitelo, para verificar.'
+    }    
+
+    username = forms.CharField(label=_(u'Usuario'), max_length=30, 
+                                widget=forms.TextInput(
+                                attrs={'class':'input-text', 'placeholder':'Uusario'}), 
+                                help_text = ayudas['username'])
+    password1 = forms.CharField(label=_("Password"), 
+                                widget=forms.PasswordInput(
+                                attrs={'class':'input-text', 'placeholder':'Contraseña'}), 
+                                   help_text=ayudas['password1'])
+    class Meta:
+        model = User
+        fields = ("username", "password1")
