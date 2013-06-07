cc0ae38fefc6ff674b60dea9c511d186c6a7f39b
Carlos Do Carmo <cdocarmo@cdsoft.com.uy>
2013-06-07 12:32:39 -0300

    Todo

---
+++ cdtareas/userprofile/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from models import *
+
+class UserAdmin(admin.ModelAdmin):
+	fieldsets = (
+			(None, {
+					'fields': (('user', 'telefono', 'celular'), 
+						('documento', 'mail_profesional'), 'domicilio')
+				}),
+		)
+	list_display = ['user', 'telefono', 'celular', 'mail_profesional']
+	search_fields = ('user', )
+	ordering = ('user', )
+
+
+
+admin.site.register(UserProfile, UserAdmin)
